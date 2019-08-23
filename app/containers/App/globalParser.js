67efc74800921c3cd332e6c15948d550eed7589e
Dio-zhu <zzjbarry@126.com>
2019-08-23 09:51:34 +0800

    Initial commit

---
+++ app/containers/App/globalParser.js
@@ -0,0 +1,15 @@
+import {Row,Col,Button} from 'tinper-bee';
+import PageLayout from 'bee-page-layout';
+
+export default {
+    "Button":Button ,
+    "PageLayout":PageLayout ,
+    "PageLayout.Header":PageLayout.Header,
+    "PageLayout.Content": PageLayout.Content,
+    "PageLayout.SearchArea": PageLayout.SearchArea,
+    "PageLayout.TableContent": PageLayout.TableContent,
+    "PageLayout.LeftContent": PageLayout.LeftContent,
+    "PageLayout.RightContent": PageLayout.RightContent,
+    "Row":Row ,
+    "Col":Col
+};
---
+++ app/containers/App/index.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {CreateView, connect} from 'zzj-genview';
+
+import './index.css';
+import globalParser from "./globalParser";
+
+
+const WrappedCreateView = connect(CreateView);
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+      return <WrappedCreateView globalParser={globalParser}/>
+    // return (
+    //     <div className='u-container'>
+    //         <div className='u-row'>
+    //             <div className='u-col-md-4 u-col-md-offset-4 border'>
+    //                 <div className='u-col-md-12 margin-20'>
+    //                     <FormControl  placeholder="请输入用户名" onChange={this.HanderChange} />
+    //
+    //                 </div>
+    //                 <div className='u-col-md-12 margin-20'>
+    //                     <FormControl placeholder="请输入密码" onChange={this.HanderChange} />
+    //                 </div>
+    //                 <div className='u-col-md-12 margin-20'>
+    //                     <div className='u-col-md-4'>
+    //                         <Button type="primary">登陆</Button>
+    //                     </div>
+    //                     <div className='u-col-md-4'>
+    //                         <Button type="success">注册</Button>
+    //                     </div>
+    //
+    //                 </div>
+    //         </div>
+    //     </div>
+    // </div>
+    // );
+  }
+}
+
+export default App;
